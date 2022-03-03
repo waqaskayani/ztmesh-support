e7095fdb0089bd957e4c53f4440a66a64fe18e5b
Waqas Kayani <mvickykayani@gmail.com>
2022-03-03 15:56:10 +0500

    Update ztmesh-azure-multicloud-script.sh

--- ztmesh-azure-multicloud-script.sh
+++ ztmesh-azure-multicloud-script.sh
@@ -3,7 +3,7 @@
 INSTANCE_NAME=$1
 VAULT_NAME=$2
 BUILD_ENV=$3
-PATH=/root/.relay-agent
+FILEPATH=/root/.relay-agent
 
 apt update -y
 apt install jq -y
@@ -11,8 +11,8 @@ apt install jq -y
 # Get access token for Azure Vault audience
 access_token=$(curl -s 'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https://vault.azure.net' -H Metadata:true | jq -r '.access_token')
 
-mkdir -p /root/.relay-agent
-cat >$PATH/config.json << 'EOL'
+mkdir -p $FILEPATH
+cat >$FILEPATH/config.json << 'EOL'
 {
   "login_config": {
     "access_key_id": "value_1",
@@ -24,16 +24,16 @@ cat >$PATH/config.json << 'EOL'
   }
 }
 EOL
-cat >$PATH/instance_name << 'EOL'
+cat >$FILEPATH/instance_name << 'EOL'
 instance_name
 EOL
 
-sed -i "s~instance_name~${INSTANCE_NAME}~g" $PATH/instance_name
+sed -i "s~instance_name~${INSTANCE_NAME}~g" $FILEPATH/instance_name
 
-sed -i "s~value_1~$(curl -s "https://${VAULT_NAME}.vault.azure.net/secrets/access-key-id?api-version=2016-10-01" -H "Authorization: Bearer $access_token" | jq -r '.value')~g" $PATH/config.json
-sed -i "s~value_2~$(curl -s "https://${VAULT_NAME}.vault.azure.net/secrets/secret-access-key?api-version=2016-10-01" -H "Authorization: Bearer $access_token" | jq -r '.value')~g" $PATH/config.json
-sed -i "s~value_3~$(curl -s "https://${VAULT_NAME}.vault.azure.net/secrets/site-id?api-version=2016-10-01" -H "Authorization: Bearer $access_token" | jq -r '.value')~g" $PATH/config.json
-sed -i "s~value_4~$(curl -s "https://${VAULT_NAME}.vault.azure.net/secrets/site-name?api-version=2016-10-01" -H "Authorization: Bearer $access_token" | jq -r '.value')~g" $PATH/config.json
+sed -i "s~value_1~$(curl -s "https://${VAULT_NAME}.vault.azure.net/secrets/access-key-id?api-version=2016-10-01" -H "Authorization: Bearer $access_token" | jq -r '.value')~g" $FILEPATH/config.json
+sed -i "s~value_2~$(curl -s "https://${VAULT_NAME}.vault.azure.net/secrets/secret-access-key?api-version=2016-10-01" -H "Authorization: Bearer $access_token" | jq -r '.value')~g" $FILEPATH/config.json
+sed -i "s~value_3~$(curl -s "https://${VAULT_NAME}.vault.azure.net/secrets/site-id?api-version=2016-10-01" -H "Authorization: Bearer $access_token" | jq -r '.value')~g" $FILEPATH/config.json
+sed -i "s~value_4~$(curl -s "https://${VAULT_NAME}.vault.azure.net/secrets/site-name?api-version=2016-10-01" -H "Authorization: Bearer $access_token" | jq -r '.value')~g" $FILEPATH/config.json
 
 # curl -L bit.ly/relayagent | sudo bash -s ${BUILD_ENV}
 # relay-agent-cli start
