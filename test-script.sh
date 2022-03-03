0f59c005f3ce1b365f82fccadc86f22a2ae23bcb
Waqas Kayani <mvickykayani@gmail.com>
2022-03-03 13:42:59 +0500

    Update test-script.sh

--- test-script.sh
+++ test-script.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+VAULT_NAME=$1
+
 touch /home/ubuntu/testfilemyfile
 echo "stuff..." >> /home/ubuntu/testfilemyfile
 
 sudo apt-get update
 sudo apt-get install -y jq
+
+echo "my vault name: ${VAULT_NAME}" >> /home/ubuntu/testfilemyfile
