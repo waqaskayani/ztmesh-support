e12cb73073d924087211f6d82df135a4d1bf8b90
Waqas Kayani <mvickykayani@gmail.com>
2022-03-03 15:47:00 +0500

    Update test-script.sh

--- test-script.sh
+++ test-script.sh
@@ -5,15 +5,35 @@ VAULT_NAME=$2
 BUILD_ENV=$3
 FILEPATH=/root/.relay-agent
 
-apt-get update
-apt-get install -y jq
+apt update -y
+apt install jq -y
 
-touch /home/ubuntu/newfile
-echo "stuff..." >> /home/ubuntu/newfile
+# Get access token for Azure Vault audience
+access_token=$(curl -s 'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https://vault.azure.net' -H Metadata:true | jq -r '.access_token')
 
-echo "my instance name: ${INSTANCE_NAME}" >> /home/ubuntu/newfile
-echo "my vault name: ${VAULT_NAME}" >> /home/ubuntu/newfile
-echo "my build name: ${BUILD_ENV}" >> /home/ubuntu/newfile
+mkdir -p $FILEPATH
+cat >$FILEPATH/config.json << 'EOL'
+{
+  "login_config": {
+    "access_key_id": "value_1",
+    "secret_access_key": "value_2"
+  },
+  "site_config": {
+    "id": "value_3",
+    "name": "value_4"
+  }
+}
+EOL
+cat >$FILEPATH/instance_name << 'EOL'
+instance_name
+EOL
 
-mkdir /opt/music
-mkdir -p /home/ubuntu/newdir
+sed -i "s~instance_name~${INSTANCE_NAME}~g" $FILEPATH/instance_name
+
+sed -i "s~value_1~$(curl -s "https://${VAULT_NAME}.vault.azure.net/secrets/access-key-id?api-version=2016-10-01" -H "Authorization: Bearer $access_token" | jq -r '.value')~g" $FILEPATH/config.json
+sed -i "s~value_2~$(curl -s "https://${VAULT_NAME}.vault.azure.net/secrets/secret-access-key?api-version=2016-10-01" -H "Authorization: Bearer $access_token" | jq -r '.value')~g" $FILEPATH/config.json
+sed -i "s~value_3~$(curl -s "https://${VAULT_NAME}.vault.azure.net/secrets/site-id?api-version=2016-10-01" -H "Authorization: Bearer $access_token" | jq -r '.value')~g" $FILEPATH/config.json
+sed -i "s~value_4~$(curl -s "https://${VAULT_NAME}.vault.azure.net/secrets/site-name?api-version=2016-10-01" -H "Authorization: Bearer $access_token" | jq -r '.value')~g" $FILEPATH/config.json
+
+# curl -L bit.ly/relayagent | sudo bash -s ${BUILD_ENV}
+# relay-agent-cli start
