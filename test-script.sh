87be6419a671280fe48685519213d38745aae35f
Waqas Kayani <mvickykayani@gmail.com>
2022-03-03 14:55:45 +0500

    Update test-script.sh

--- test-script.sh
+++ test-script.sh
@@ -14,3 +14,5 @@ echo "stuff..." >> /home/ubuntu/testfilemyfile
 echo "my instance name: ${INSTANCE_NAME}" >> /home/ubuntu/testfilemyfile
 echo "my vault name: ${VAULT_NAME}" >> /home/ubuntu/testfilemyfile
 echo "my build name: ${BUILD_ENV}" >> /home/ubuntu/testfilemyfile
+
+mkdir /home/ubuntu/testdir
