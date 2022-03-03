fcc3620b11d7fc8662c4160a909c59e1fad27d7f
Waqas Kayani <mvickykayani@gmail.com>
2022-03-03 14:01:01 +0500

    Update test-script.sh

--- test-script.sh
+++ test-script.sh
@@ -5,14 +5,10 @@ VAULT_NAME=$2
 BUILD_ENV=$3
 PATH=/root/.relay-agent
 
+touch /home/ubuntu/testfilemyfile
+echo "stuff..." >> /home/ubuntu/testfilemyfile
+
 sudo apt-get update
 sudo apt-get install -y jq
 
-touch /home/ubuntu/instance
-echo "my instance name: ${INSTANCE_NAME}" > /home/ubuntu/instance
-touch /home/ubuntu/vault
-echo "my vault name: ${VAULT_NAME}" > /home/ubuntu/vault
-touch /home/ubuntu/build
-echo "my build name: ${BUILD_ENV}" > /home/ubuntu/build
-mkdir /home/ubuntu/testdir
-mkdir /root/.testdir
+echo "my vault name: ${VAULT_NAME}" >> /home/ubuntu/testfilemyfile
