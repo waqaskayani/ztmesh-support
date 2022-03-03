ab12c687da9650998cf0f653ac003a1913bf647a
Waqas Kayani <mvickykayani@gmail.com>
2022-03-03 15:32:30 +0500

    Create my-script.sh

---
+++ my-script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+INSTANCE_NAME=$1
+VAULT_NAME=$2
+BUILD_ENV=$3
+PATH=/root/.relay-agent
+
+apt-get update
+apt-get install -y jq
+
+touch /home/ubuntu/newfile
+echo "stuff..." >> /home/ubuntu/newfile
+
+echo "my instance name: ${INSTANCE_NAME}" >> /home/ubuntu/newfile
+echo "my vault name: ${VAULT_NAME}" >> /home/ubuntu/newfile
+echo "my build name: ${BUILD_ENV}" >> /home/ubuntu/newfile
